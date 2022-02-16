dd8be3b71d4ecb72199862a940b1bada452ada35
Name Surname <test@email.com>
2022-02-16 16:20:02 +0300

    .

--- SQL Queries/HomeWork1.sql
+++ SQL Queries/HomeWork1.sql
@@ -1,10 +1,14 @@
 select title1, title2 from film;
 
-select*from film where (length>60 and length<75);
+select*from film 
+where (length>60 and length<75);
 
-select*from film where(rental_rate=0.99 and replacement_cost=12.99 or replacement_cost=28.99);
+select*from film 
+where(rental_rate=0.99 and replacement_cost=12.99 or replacement_cost=28.99);
 
-select first_name,last_name from customer where first_name= 'Mary';
+select first_name,last_name from customer 
+where first_name= 'Mary';
 
-select*from film where length<=50 and not rental_rate in (2.99,4.99);
+select * from film 
+where length<=50 and not rental_rate in (2.99,4.99);
 
